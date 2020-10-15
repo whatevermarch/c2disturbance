838c8d4eaa36350447b94866d3bb2fd3fbed0979
Krittin Meenrattanakorn <whatevermarch@gmail.com>
2020-10-15 22:17:04 +0200

    Fix path resolving

--- blender/scripts/main_render.py
+++ blender/scripts/main_render.py
@@ -5,20 +5,20 @@ import logging
 
 
 #   number of samples (undistorted images)
-N = 1
+N = 2
 
 #   frame interval
 frame_start = 1
-frame_end = 5
+frame_end = 3
 
 #   sample file expression to be formatted later
 sample_name = "{:03d}.png"
 
 #   define sample directory and its name (expression)
-sample_dir = bpy.path.abspath( '//../../data/samples' )
+sample_dir = os.path.abspath( bpy.path.abspath( '//../../data/samples' ) )
 
 #   define output directory
-output_dir = bpy.path.abspath( '//../../data/distorted' )
+output_dir = os.path.abspath( bpy.path.abspath( '//../../data/distorted' ) )
 
 #   setup logging level
 #logging.basicConfig( level=logging.DEBUG )
@@ -27,11 +27,11 @@ output_dir = bpy.path.abspath( '//../../data/distorted' )
 #   setup the environment before rendering
 def init():
 
-    logging.debug( "Initializing..." )
-    logging.debug( "\tNumber of samples : {}".format( N ) )
-    logging.debug( "\tRender from frame {} to frame {}".format( frame_start, frame_end ) )
-    logging.debug( "\tRetreive samples from : {}".format( sample_dir ) )
-    logging.debug( "\tSave outputs to : {}".format( output_dir ) )
+    print( "Initializing..." )
+    logging.debug( "Number of samples : {}".format( N ) )
+    logging.debug( "Render frame {} -> {}".format( frame_start, frame_end ) )
+    logging.debug( "Retrieve samples from : {}".format( sample_dir ) )
+    logging.debug( "Save outputs to : {}".format( output_dir ) )
 
     #   define animation frame range to be rendered
     bpy.context.scene.frame_start = frame_start
@@ -47,8 +47,8 @@ def init():
         if cycles_pref.has_active_device():
             break
 
-    logging.debug( "\tRendering API : {}".format( cycles_pref.compute_device_type ) )
-    logging.debug( "\tDevice(s) in use :" )
+    logging.debug( "Rendering API : {}".format( cycles_pref.compute_device_type ) )
+    logging.debug( "Device(s) in use :" )
         
     #   enable appropriate devices
     #   for CUDA, use all available GPUs except host CPU
@@ -58,22 +58,18 @@ def init():
                 device.use = False
             else:
                 device.use = True
-                logging.debug( "\t- {}".format( device.name ) )
+                logging.debug( "\t{}".format( device.name ) )
             
     #   for OPENCL (usually a single-GPU system), enable both CPU and GPU 
     elif cycles_pref.compute_device_type == 'OPENCL':
         for device in cycles_pref.devices:
             device.use = True
-            logging.debug( "\t- {}".format( device.name ) )
-
-    logging.debug( "" )
+            logging.debug( "\t{}".format( device.name ) )
 
 
 #   render, ain't nothing else
 def render():
 
-    logging.debug( "Start rendering..." )
-    
     #   define target texture node
     materials = bpy.data.materials
     tex_node = materials['Material.Text'].node_tree.nodes["Image Texture"]
@@ -90,14 +86,14 @@ def render():
     scene = bpy.data.scenes['Scene']
 
     #   loop all over the required samples
-    for sample_idx in range(N):
+    for sample_idx in range( N ):
 
-        logging.debug( "Render sample [{} out of {}]".format( sample_idx, N ) )
+        print( "Rendering sample [{} out of {}]...".format( sample_idx, N ) )
 
-        logging.debug( "\tloading texture..." )
+        logging.debug( "loading texture..." )
 
         #   load new image
-        new_img = images.load( os.path.join( sample_dir, sample_name.format( sample_idx ) ) )
+        new_img = images.load( os.path.join( sample_dir, sample_name.format( sample_idx ) ), check_existing=True )
 
         #   replace the existing image
         old_img = tex_node.image
@@ -106,7 +102,7 @@ def render():
         #   remove used image from the database to save memory space
         images.remove( old_img )
 
-        logging.debug( "\tinitialize animation parameter..." )
+        logging.debug( "initialize animation parameter..." )
 
         #   setup W-param for this sample
         #   randomize the initial values
@@ -115,28 +111,29 @@ def render():
         m2_start = random.uniform( 0.0, 50.0 )
         m2_end = m2_start + random.uniform( 20.0, 50.0 ) * ( -1 ) ** random.randint( 0, 1 )
         #   bound to keyframe
-        bpy.context.scene.frame_set( frame_start )
+        bpy.context.scene.frame_set( 1 )
         musgrave_1.inputs[1].default_value = m1_start
         musgrave_1.inputs[1].keyframe_insert( data_path="default_value" )
         musgrave_2.inputs[1].default_value = m2_start
         musgrave_2.inputs[1].keyframe_insert( data_path="default_value" )
-        bpy.context.scene.frame_set( frame_end )
+        bpy.context.scene.frame_set( 100 )
         musgrave_1.inputs[1].default_value = m1_end
         musgrave_1.inputs[1].keyframe_insert( data_path="default_value" )
         musgrave_2.inputs[1].default_value = m2_end
         musgrave_2.inputs[1].keyframe_insert( data_path="default_value" )
 
-        logging.debug( "\trender..." )
+        logging.debug( "\tMusgrave[0] : {} -> {} ".format( m1_start, m1_end ) )
+        logging.debug( "\tMusgrave[1] : {} -> {} ".format( m2_start, m2_end ) )
+
+        logging.debug( "render..." )
 
         #   set the output subdirectory by sample index
-        scene.render.filepath = os.path.join( output_dir, sample_idx, "" )
+        scene.render.filepath = os.path.join( output_dir, str( sample_idx ), "" )
 
         #   render animation
         bpy.ops.render.render( animation=True, write_still=True )
 
-        logging.debug( "Sample [{}] completed!".format( sample_idx ) )
-
-    logging.debug( "" )
+        print( "Sample [{}] completed!".format( sample_idx ) )
 
 
 if __name__ == "__main__":
